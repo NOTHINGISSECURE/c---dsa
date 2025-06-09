1fd39c9850b46586dd875124e6cdd18a5f4975ce
Akash Tiwari <tiwarikiran824@gmail.com>
2025-06-09 16:10:29 +0530

    dsa

---
+++ array/syntaxanddeclaration.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x[7];
+    // for initializing the array
+    for(int i=0;i<=6;i++){
+        cin>>x[i];
+    }
+    // for printing the array
+    for(int i=0;i<=6;i++){
+        cout<<x[i]<<" ";
+    }
+
+}
